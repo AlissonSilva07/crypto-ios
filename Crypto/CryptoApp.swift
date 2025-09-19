042d2f7f168d8d1510afc28d5386ff537aeebb25
Alisson Rego <alisson.rego@icc.org.br>
2025-09-19 08:13:03 -0300

    Initial Commit

---
+++ Crypto/CryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoApp.swift
+//  Crypto
+//
+//  Created by Liv03 on 19/09/25.
+//
+
+import SwiftUI
+
+@main
+struct CryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
