f779deea15cbcc78d6aef106b5362dbf3e27ab35
Alisson Rego <alisson.rego@icc.org.br>
2025-09-19 08:26:28 -0300

    chore: first commit

---
+++ Crypto/Extensions/Color.swift
@@ -0,0 +1,17 @@
+//
+//  Color.swift
+//  Crypto
+//
+//  Created by Liv03 on 19/09/25.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let theme = ColorTheme()
+}
+
+struct ColorTheme {
+    let accent = Color("AccentColor")
+}
