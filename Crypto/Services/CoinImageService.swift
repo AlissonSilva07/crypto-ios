352dd79112315d45e5ffe051b958c530f99531ef
Alisson Rego <alisson.rego@icc.org.br>
2025-09-22 15:15:35 -0300

    feat: cache images before downloading them

--- Crypto/Services/CoinImageService.swift
+++ Crypto/Services/CoinImageService.swift
@@ -12,24 +12,39 @@ import Combine
 class CoinImageService {
     
     @Published var image: UIImage? = nil
-    var imageSubscription: AnyCancellable?
-    private let coin: Coin
-    
-    init (coin: Coin) {
-        self.coin = coin
-        getCoinImage()
-    }
-    
-    private func getCoinImage() {
-        guard let url = URL(string: coin.image) else { return }
         
-        imageSubscription = NetworkingManager.download(url: url)
-            .tryMap({ (data) -> UIImage? in
-                return UIImage(data: data)
-            })
-            .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self] (returnedImage) in
-                self?.image = returnedImage
-                self?.imageSubscription?.cancel()
-            })
-    }
-}
+        private var imageSubscription: AnyCancellable?
+        private let coin: Coin
+        private let fileManager = LocalFileManager.instance
+        private let folderName = "coin_images"
+        private let imageName: String
+        
+        init(coin: Coin) {
+            self.coin = coin
+            self.imageName = coin.id
+            getCoinImage()
+        }
+        
+        private func getCoinImage() {
+            if let savedImage = fileManager.getImage(imageName: imageName, folderName: folderName) {
+                image = savedImage
+            } else {
+                downloadCoinImage()
+            }
+        }
+        
+        private func downloadCoinImage() {
+            guard let url = URL(string: coin.image) else { return }
+            
+            imageSubscription = NetworkingManager.download(url: url)
+                .tryMap({ (data) -> UIImage? in
+                    return UIImage(data: data)
+                })
+                .receive(on: DispatchQueue.main)
+                .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self] (returnedImage) in
+                    guard let self = self, let downloadedImage = returnedImage else { return }
+                    self.image = downloadedImage
+                    self.imageSubscription?.cancel()
+                    self.fileManager.saveImage(image: downloadedImage, imageName: self.imageName, folderName: self.folderName)
+                })
+        }}
